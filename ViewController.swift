641c7681bfd85487c7e10b5fa0475e8b5d71ac36
ale-drian <thalia.adrian@tecsup.edu.pe>
2021-05-17 01:49:14 -0500

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdrianColeccionDeJuegos
+//
+//  Created by Mac 14 on 5/12/21.
+//  Copyright © 2021 Mac 14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
